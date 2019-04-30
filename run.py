3638b95a6d346b553eeb62ce6020ce536dd1683d
Teagan Mathur <tmathur2@illinois.edu>
2019-04-29 23:20:19 -0500

    Added soup2

--- run.py
+++ run.py
@@ -4,4 +4,6 @@ from bs4 import BeautifulSoup
 
 response = requests.get(sys.argv[1])
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.find_all("th", string="Music by")[0].parent.td.a.string)
+soup2 = BeautifulSoup(str(soup.find_all("th", string="Music by")[0].parent.td), 'html.parser')
+for name in soup2.find_all("a"):
+    print(name.string)
